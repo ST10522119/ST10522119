39e933769dc0bf401358d1b61174c08c87e39ab7
ST10522119 <ST10522119@myemeris.edu.za>
2026-06-25 11:01:06 +0200

    Bibliography

--- POE/src/main/java/PROG5121_POE_ST10522119.java
+++ POE/src/main/java/PROG5121_POE_ST10522119.java
@@ -405,19 +405,3 @@ private static void storedMessagesMenu(Scanner input, Message reportMessage)
            }
           }
          }
-/*
-Bibliography
-
-Cengage Learning, 2019a. Java Programming, 9e: Chapter 3: Using Methods, Classes, and Objects. Boston: Cengage Learning.
-
-Cengage Learning, 2019b. Java Programming, 9e: Chapter 4: More Object Concepts. Boston: Cengage Learning.
-
-Cengage Learning, 2019c. Java Programming, 9e: Chapter 5 and Chapter 6: Making Decisions and Looping. Boston: Cengage Learning.
-
-Cengage Learning, 2019d. Java Programming, 9e: Chapter 7: Characters, Strings, and the StringBuilder. Boston: Cengage Learning.
-
-Cengage Learning, 2019e. Java Programming, 9e: Chapter 8: Arrays. Boston: Cengage Learning.
-
-Farrell, J., 2023. Java Programming. Boston: Cengage Learning.
-
-*/
